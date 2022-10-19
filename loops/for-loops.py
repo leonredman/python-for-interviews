71a139845ef32f67d370825383427f4fdc3ad9f3
leonredman <leonproart@yahoo.com>
2022-10-18 23:13:37 -0700

    init commit

---
+++ loops/for-loops.py
@@ -0,0 +1,14 @@
+# for loops - syntax is a bit tricky vs explicit loop for(int i = 0; i < n; i++)
+# Looping from i = 0 to i = 4  ( i goes in the range of 5, so starts at 0 and ends at 4) 
+# i is incremented implicitly - dont have to tell loop to increment by one by default
+for i in range(5):
+  print(i)
+# yeilds 0 1 2 3 4
+
+# we go from 2 to 5 so we pass in those values, starting at 2 and going to 6 but not including 6
+for i in range(2, 6):
+  print(i)
+
+# to go in reverse we pass in a 3rd value of -1 because we going backwards(decrement) or -2 to go by 2
+for i in range(5, 1, -1):
+  print(i)
---
+++ loops/while-loops.py
@@ -0,0 +1,7 @@
+# while loops are very similar
+# conditionals do not have to go in parentheses, we indent tab or 5 spaces and use a colon after conditional
+
+n = 0
+while n < 5:
+     print(n)
+     n += 1   
