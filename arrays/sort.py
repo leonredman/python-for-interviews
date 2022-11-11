b3d070a277b0ba1257de12d743d5781b802aa351
leonredman <leonproart@yahoo.com>
2022-11-10 23:27:11 -0700

    sort updates

--- arrays/sort.py
+++ arrays/sort.py
@@ -15,5 +15,11 @@ print(arrString)
 
 #yeild ['alice', 'bob', 'doe' 'jane']
 
-# Custom sort (by length of string) an use a lambda
+# Custom sort (by length of string) we cam pass in a lambda function
+# basically a function without a name the key is equal to lambda. We take every single 
+# value from the array call it x then return from that the length of x and this is the key
+# that is used to sort the string. Each string is mapped to its length and 
+# then we sort those strings based on their length. By default will be in ascending order
+# 
 arr.sort(key=lambda x: len(x))
+# yeild ['alice', 'bob', 'doe', 'jame']
